ed4f24c216f4d752de3e30391b59865c1d03fc50
anushachuppala <anusha.chuppala12@gmail.com>
2026-07-13 21:04:45 +0530

    Inline CSS styling

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,5 +1,14 @@
 function Button() {
-  return <button class="button">Click me</button>;
+  const styles = {
+    backgroundColor: "hsl(200, 100%, 50%)",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+  };
+
+  return <button style={styles}>Click me</button>;
 }
 
 export default Button;
